65390bd9ddbe5c1b597b52bb5f4cf1ae2cd673e5
Ana Xavier <analiviaxavier333@gmail.com>
2023-04-13 21:41:02 -0300

    Exercicios de condicionais

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+
+
 // Fahrenheit para Kelvin
 console.log("ESERCICIO 1")
+
 // 77 graus fahrenheit para kelvin
 console.log("a)")
 let kelvin = (77-32)*(5/9)+ 273.15
@@ -27,5 +30,49 @@ console.log(valorCelsius+" graus Celsius para Fahrenheit: "+celsiusToFahrenheit+
 fahrenheitToKelvin = (celsiusToFahrenheit-32)*(5/9)+ 273.15
 console.log(valorCelsius+" graus Celsius para Kelvin: "+fahrenheitToKelvin+ "K")
 
+
+// DIA 13/04
+
+
+console.log("")
+console.log("Exercicio: verificar 3 notas e média=7")
+nome = prompt('Informe o nome do usuário')
+nota1 = Number(prompt('Informe a primeira nota:'))
+nota2 = Number(prompt('Informe a segunda nota:'))
+nota3 = Number(prompt('Informe a terceira nota:'))
+
+const media = (nota1 + nota2 + nota3)/3
+
+console.log('O/A aluno/a', nome, 'teve média: ', media)
+
+console.log("Aprovação:",media>=7)
+
+if(media>=7){
+    console.log("Parabéns")
+} else if(media<=6){
+    console.log("Reprovado")
+}
+
+
+console.log('')
+console.log("Exercicio: número par ou ímpar")
+n = prompt("Informe um número")
+if(n % 2 == 0){
+    console.log(n,"é par")
+}else{
+    console.log(n,"é ímpar")
+}
+
+
+console.log("")
+console.log("Exercicio: qual número é maior")
+n1 = prompt("Informe o primeiro número")
+n2 = prompt("Informe o segundo número")
+
+if(n1>n2){
+    console.log(n1, "é maior")
+} else{
+    console.log(n2,"é maior")
+}
 
 
